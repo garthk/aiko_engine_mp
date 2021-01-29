254c7145afd2b3bb2255783785db8cc6241acb53
Garth Kidd <garth@garthk.com>
2021-01-30 10:29:11 +1100

    Reconfigure LEDs

--- configuration/led.py
+++ configuration/led.py
@@ -3,13 +3,15 @@
 settings = {    # LED panel or string
   "apa106":       False,  # IoTuz and LoliBot use APA106 RGB LEDs
 # "dimension":    (1,),
-  "dimension":    (8,),   # Edge lit acrylic panel
+# "dimension":    (8,),   # Edge lit acrylic panel
+  "dimension":    (4,),   # Hand-cut WS2812 strip
 # "dimension":    (8, 8),
 # "dimension":    (32, 8),
 # "dimension":    (32, 32),
 # "neopixel_pin":  4,     # TinyPICO
-  "neopixel_pin": 13,     # Usually
+# "neopixel_pin": 13,     # Usually
 # "neopixel_pin": 15,     # Wemos OLED
 # "neopixel_pin": 23,     # IoTuz
+  "neopixel_pin": 19,     # SAO_1 bottom right (from front of badge)
   "zigzag":       False   # For 2D panels
 }
